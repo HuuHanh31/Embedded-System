82893b40862b53b75c99e8b7fcf1bb07fc1668c3
Hanh Huynh <huuhanhce2001@gmail.com>
2022-12-04 15:58:09 +0700

    small fix

--- Lab_5/main/main.c
+++ Lab_5/main/main.c
@@ -1,6 +1,74 @@
+#include "freertos/FreeRTOS.h"
+#include "freertos/queue.h"
+#include "freertos/task.h"
+#include "freertos/timers.h"
+#include "sys/time.h"
+#include <inttypes.h>
 #include <stdio.h>
+
+#define TIMER_MESSEAGE_1 "ahihi"
+#define TIMER_MESSEAGE_2 "ihaha"
+#define TIMER_ID_1 (0)
+#define TIMER_ID_2 (1)
+#define TIMER_REPEAT_1 (10)
+#define TIMER_REPEAT_2 (5)
+#define TIMER_PERIOD_1_MS (2000)
+#define TIMER_PERIOD_2_MS (3000)
+
+typedef struct
+{
+    uint32_t timer_elapsed_count;
+    uint32_t id;
+} Timer_info_t;
+
+TimerHandle_t timer[2];
+Timer_info_t info[2] = {0};
+
+static void print_current_time()
+{
+    struct timeval tv_now;
+    gettimeofday(&tv_now, NULL);
+    int64_t time_us = (int64_t)tv_now.tv_sec * 1000000L + (int64_t)tv_now.tv_usec;
+    printf("%lld", time_us);
+}
+
+void print_message_callback(TimerHandle_t xTimer)
+{
+    Timer_info_t *info = (Timer_info_t *)pvTimerGetTimerID(xTimer);
+
+    if (info->id == TIMER_ID_1)
+    {
+        print_current_time();
+        printf(TIMER_MESSEAGE_1 "\n");
+        info->timer_elapsed_count++;
+        if (info->timer_elapsed_count == TIMER_REPEAT_1)
+        {
+            xTimerStop(xTimer, 0);
+        }
+    }
+    else if (info->id == TIMER_ID_2)
+    {
+        print_current_time();
+        printf(TIMER_MESSEAGE_2 "\n");
+        info->timer_elapsed_count++;
+        if (info->timer_elapsed_count == TIMER_REPEAT_2)
+        {
+            xTimerStop(xTimer, 0);
+        }
+    }
+}
 
 void app_main(void)
 {
+    info[0].timer_elapsed_count = 0;
+    info[0].id = TIMER_ID_1;
+    timer[0] = xTimerCreate("Timer 1", pdMS_TO_TICKS(TIMER_PERIOD_1_MS), pdTRUE,
+                            (void *)&info[0], print_message_callback);
 
+    info[1].timer_elapsed_count = 0;
+    info[1].id = TIMER_ID_2;
+    timer[1] = xTimerCreate("Timer 2", pdMS_TO_TICKS(TIMER_PERIOD_2_MS), pdTRUE,
+                            (void *)&info[1], print_message_callback);
+
+    vTaskDelete(NULL);
 }
